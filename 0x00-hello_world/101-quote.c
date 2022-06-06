e573c276e4d69c182b84f3307b2564335273f212
mattmuange <muange82@gmail.com>
2022-06-05 21:09:43 -0700

    betty 3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,30 +2,18 @@
 
 #include <unistd.h>
 
-
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *       */
-
-
+* main -use to return the integer value 1
+*
+*Description:use to print to standard output in the main function
+*
+* Return:return the value 1
+*
+*/
 
 int main(void)
 
 {
-
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
-
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
